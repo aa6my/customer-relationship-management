3c84f17fe7b382b97f6eabc7fbf9784162bdcf58
norlihazmey <norlihazmey89@gmail.com>
2014-11-11 13:43:32 +0800

    midae_model.php ==> adding some function, see the comments in code

--- application/models/midae_model.php
+++ application/models/midae_model.php
@@ -110,14 +110,17 @@ class Midae_model extends CI_Model {
         return object();
     }
 
+
     private function _formatDate($date)
     {
         return strtotime(substr($date, 6, 4)."-".substr($date, 0, 2)."-".substr($date, 3, 2)." " .substr($date, 10, 6)) * 1000;
     }
 
+
+
     /**
     * [get_all_rows This function can use for all the tables, default function is to call all the results rows in table]
-    * @param  [type] $table [name of table you want to fetch data]
+    * @param  [string] $table [name of table you want to fetch data]
     * @return [type]        [return data sets]
     */
     function get_all_rows($table)
@@ -137,9 +140,9 @@ class Midae_model extends CI_Model {
 
     /**
      * [get_specified_row This function can use for all the tables, default function is to call the specified results rows in table]
-     * @param  [type] $table [name of table you want to fetch data]
-     * @param  [type] $where [where condition]
-     * @param  [type] $order_by [order by]
+     * @param  [string] $table [name of table you want to fetch data]
+     * @param  [array] $where [where condition]
+     * @param  [array] $order_by [order by]
      * @return [type] [return sepcified row]
      */
     function get_specified_row($table,$where,$order_by)
@@ -152,7 +155,6 @@ class Midae_model extends CI_Model {
         {
              $this->db->where($where); 
         }
-       
 
         if($order_by!=false)
         {
@@ -161,6 +163,59 @@ class Midae_model extends CI_Model {
 
         $query = $this->db->get();
         return $query->row_array();    
+    }
+
+
+
+
+    /**
+     * [update_data update datas in any tables you want]
+     * @param  [array] $columnToUpdate [what column you want to update = array value]
+     * @param  [string] $tableToUpdate  [what table you want to update]
+     * @param  [array] $usingCondition [using condition or not]
+     * @return [type]                 [description]
+     */
+    function update_data($columnToUpdate, $tableToUpdate, $usingCondition)
+    {
+        
+        $this->db->where($usingCondition);
+        $this->db->update($tableToUpdate, $columnToUpdate);
+
+
+    }
+
+
+
+    /**
+     * [display_message display flash message data in view part]
+     * @param  [string] $messageType [what type of message you want to display]
+     * @param  [string] $urlToGo     [url you want to redirect after making the process ==> if using current url just use current_url()]
+     * @return [type]              [description]
+     */
+    function display_message($messageType, $urlToGo)
+    {
+        /**
+         * usage :
+         * calling this method in controller of course
+         * example : display_message("save","jobs/index/add")
+         * @var [type]
+         */
+        if($messageType=="save")
+        {
+             $this->session->set_flashdata('save', 'Successfully saved');
+
+        }
+        else if($messageType=="record")
+        {
+             $this->session->set_flashdata('record', 'New Job successfully recorded');
+        }
+        else if($messageType=="error")
+        {
+             $this->session->set_flashdata('error', 'There is an errors in processing, please try again.');
+        }
+       
+
+       return redirect($urlToGo);
     }
 
 
