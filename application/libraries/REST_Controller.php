761596ec1ffa5559c22faf295e95f0ab57c9c837
metallurgical <norlihazmey89@gmail.com>
2014-12-02 18:14:34 +0800

    latest api restful

--- application/libraries/REST_Controller.php
+++ application/libraries/REST_Controller.php
@@ -36,7 +36,8 @@ abstract class REST_Controller extends CI_Controller
      *
      * @var array
      */
-    protected $allowed_http_methods = array('get', 'delete', 'post', 'put', 'options', 'patch', 'head');
+    //protected $allowed_http_methods = array('get', 'delete', 'post', 'put', 'options', 'patch', 'head');
+    protected $allowed_http_methods = array('get',  'delete', 'post', 'put', 'patch', 'head');
 
     /**
      * General request data and information.
